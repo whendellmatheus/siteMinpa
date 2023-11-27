9d55aa9f1b3b346c64e8120c950ca830b9edd462
whendellmatheus <134299838+whendellmatheus@users.noreply.github.com>
2023-11-27 19:54:48 -0300

    Update271123
    Update271123

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#carouselWrapper").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '#prevBtn',
+        nextArrow: '#nextBtn',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
